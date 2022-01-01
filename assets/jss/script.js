81d358577625ae0984f9d76b9a02765a085d196f
ahnjaeyung <ahn.jaeyung@gmail.com>
2021-12-31 18:51:47 -0600

    added headline, city search container, weather information jumbotron, and 5-day forecast cards section to body of index.html.

--- assets/jss/script.js
+++ assets/jss/script.js
@@ -1,7 +1,7 @@
 console.log("hello")
 var apiKey = "cce93c9a1a3559e138f0185459e4b998"
-var cityName = "Boston"
-var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+var cityName = "Minneapolis"
+var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + apiKey;
 
 fetch(requestUrl)
     .then(function (response){
